bb6b00d5b634e0e4a3b4c6fd1f6fb87b1dc8b548
Admin <Admin@192.168.1.3>
2018-12-09 20:12:32 +0530

    commiting my first project

---
+++ src/TestClass.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first program on PC...");
+
+		String myNames = "Saibaba,Ganapati,Shiv";
+		Set<String> setNames = StringUtils.isBlank(myNames) ? new LinkedHashSet<String>()
+				: new LinkedHashSet<String>(Arrays.asList(myNames.split(",")));
+
+		setNames.add("Hanuman");
+		setNames.add("Saibaba");
+
+		setNames.stream().forEach(System.out::println);
+		
+	}
+
+}
